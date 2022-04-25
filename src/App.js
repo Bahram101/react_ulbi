3db24784bfdbde8f6843bde9d7521b17e0de0e8b
bahram <bahram101@mail.ru>
2022-04-25 11:48:15 +0600

    added Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
-import "./styles/App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Posts from './pages/Posts';
+import './styles/App.css';
 
 function App() {
-    return (
-        <BrowserRouter>
-       
-                <Route path="/about">
-                    <About />
-                </Route>
-                <Route path="/posts">
-                    <Posts/>
-                </Route>
-         
-        </BrowserRouter>
-    );
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Posts />} />
+				<Route path='/about' element={<About />} /> 
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
